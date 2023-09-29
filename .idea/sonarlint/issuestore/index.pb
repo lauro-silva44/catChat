16e7e9bc5e91aa5d89fe668f6f59a36de90411c9
lauro <lauro@in8.com.br>
2023-09-29 11:31:36 +0100

    added the drawer

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+b
+2app/src/main/java/com/hfad/catchat/MainActivity.kt,a\a\aabf39a5f37e70417683b394f8f378e116b23190
+c
+3app/src/main/java/com/hfad/catchat/InboxFragment.kt,6\e\6ebe2dee1b2d9adc187f73befab7d0a9ca5b1a12
+b
+2app/src/main/java/com/hfad/catchat/HelpFragment.kt,5\3\53dd5c836ee2943cb86ef335662ab815a1e8ea29
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
---
+++ app/src/main/java/com/hfad/catchat/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.hfad.catchat
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val toolbar: MaterialToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar)
+
+        //finds the nav host fragment by its ID
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment;
+        //get the controller responsible for the destination pr paths
+        val navController = navHostFragment.navController;
+        //builds a configuration linking the toolbar to the navigation graph
+        val drawerLayout = findViewById<DrawerLayout>(R.id.drawer_layout);
+        val builder = AppBarConfiguration.Builder(navController.graph);
+        builder.setOpenableLayout(drawerLayout);
+        val appBarConfiguration = builder.build();
+        toolbar.setupWithNavController(navController, appBarConfiguration);
+
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_toolbar, menu)
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
+    }
+}
